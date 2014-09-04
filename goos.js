656f5da0428705b721bfbadda6d8cc96be1fa28e
Skylar Miller <skyheamillz@gmail.com>
2014-09-03 22:59:38 -0400

    modified project

--- goos.js
+++ goos.js
@@ -1,7 +1,8 @@
-document.getElementById("myBtn").onclick = function();
-var thimbles = documentgetElementByID("thimbles").value;
-var swords = documentgetElementById("swords").value;
-var thimtot = Number("thimbles")*1;
-var swordstot = Number("swords")*37;
-var total = thimtot + swordstot;
+document.getElementById("myBtn").onclick = function(){
+  var thimbles = documentgetElementByID("thimbles").value;
+  var swords = documentgetElementById("swords").value;
+  var thimtot = Number("thimbles")*1;
+  var swordstot = Number("swords")*37;
+  var total = thimtot + swordstot;
 document.getElementByID("the-total").textContent = total;
+}
